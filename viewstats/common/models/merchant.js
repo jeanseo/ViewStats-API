6f5b28728dbe20c31b025c8d5ba9aaa9c03c652d
Jean-Sébastien Merz <jsmpub@gmail.com>
2019-05-12 23:41:05 +0200

    Sync Merchant OK

--- viewstats/common/models/merchant.js
+++ viewstats/common/models/merchant.js
@@ -15,74 +15,82 @@ module.exports = function(Merchant) {
     // TODO
     let toUpdate = [];
     let toInsert = [];
-    let toPull = [];
+    let clientToUpdate = [];
+    let clientToInsert = [];
     let i=0;
-
-    data.forEach((element)=>{
-      Merchant.findById(element.id, function (err, merchant) {
-        console.log("-------------------");
-        if (err) {
-          console.log(err);
+    data.forEach((element,index)=>{
+      if (index===0){
+        let lastSync = new Date((element.lastUpdated));
+        try{
+          if (isNaN(lastSync)) throw BreakException;
+        } catch (e) {
+          callback("first object must be a valid date");
+          return;
         }
-        if (merchant) {
-          if (element.lastUpdated > merchant.lastUpdated.toISOString()) {
-            //Update
-            console.log("on fait l'update\n" + element.firstName);
-            toUpdate.push(element);
-          } else {
-            //Recupérer l'objet dans une liste
-            console.log("on garde\n" + merchant.firstName);
-            toPull.push(element);
+
+        console.log("On récupère la date");
+        console.log(typeof lastSync+lastSync);
+        Merchant.find({"where":{"creationDate" : {"gt": lastSync}}},function (err, response){
+          console.log("Erreur dans la recherche de nouvelles entrées\n"+err);
+          clientToInsert = response;
+          console.log("Résultat de la requête de populate du toInsert\n"+JSON.stringify(response));
+          i++;
+        });
+      }
+      else{
+        Merchant.findById(element.id, function (err, merchant) {
+          console.log("-------------------");
+          if (err) {
+            console.log(err);
           }
-        } else {
-          //Faire un insert
-          console.log("on crée\n" + element.firstName);
-          toInsert.push(element);
-        }
-        i++;
-        //Lorsque toutes les actions sont terminées
-        if (i===data.length){
-          Merchant.create(toInsert, function(error, response){
-            if(error) console.log(error);
-            else console.log("réponse du create:\n"+response);
-          });
+          if (merchant) {
+            if (element.lastUpdated > merchant.lastUpdated.toISOString()) {
+              //Update
+              console.log("on fait l'update\n" + element.firstName);
+              toUpdate.push(element);
+            } else if(element.lastUpdated < merchant.lastUpdated.toISOString()){
+              //Recupérer l'objet dans une liste
+              console.log("on garde\n" + merchant.firstName);
+              clientToUpdate.push(element);
+            }
+          } else {
+            //Faire un insert
+            console.log("on crée\n" + element.firstName);
+            toInsert.push(element);
+          }
+          i++;
+          //Lorsque toutes les actions sont terminées
+          if (i===data.length){
+            //On fait un Bulk Insert
+            if (toInsert.length>0){
+              Merchant.create(toInsert, function(error, response){
+                if(error) console.log(error);
+                else console.log("réponse du create:\n"+response);
+              });
+            }
+            if (toUpdate.length>0){
+              //On fait une boucle pour faire un upsert (remplace l'update)
+              toUpdate.forEach((itemToUpdate)=>{
+                console.log("on rentre dans la boucle du update");
+                i=0;
+                Merchant.upsert(itemToUpdate, function(error, response){
+                  if(error) console.log(error);
+                  else console.log("réponse du update:\n"+response);
+                  i++;
+                  if(i===toUpdate.length){
+                    console.log("upsert terminé");
+                  }
+                });
+              });
+            }
 
-          toUpdate.forEach((itemToUpdate)=>{
-            i=0;
-            Merchant.upsert(itemToUpdate, function(error, response){
-              if(error) console.log(error);
-              else console.log("réponse du update:\n"+response);
-              i++;
-              if(i===toUpdate.length){
-                console.log("-----TO UPDATE--------\n"+JSON.stringify(toUpdate)+"\n---TO INSERT---\n"+JSON.stringify(toInsert)+"\n---TO PULL---\n"+JSON.stringify(toPull));
-                callback();
-              }
-            });
-          });
-        }
-      });
+            console.log("-----TO UPDATE--------\n"+JSON.stringify(toUpdate)+"\n---TO INSERT---\n"+JSON.stringify(toInsert)+"\n---TO PULL---\n"+JSON.stringify(clientToUpdate));
+            callback(null,clientToUpdate,clientToInsert);
+          }
+        });
+      }
     });
-
-
-
-
-
-
-
-
-
-
-
   };
-
-
-
-
-
-
-
-
-
 };
 
 
