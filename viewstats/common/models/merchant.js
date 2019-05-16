93695a175d1ca6bbd9c677ec32755dd2af4e479e
Jean-Sébastien Merz <jsmpub@gmail.com>
2019-05-16 08:00:44 +0200

    Sync  OK

--- viewstats/common/models/merchant.js
+++ viewstats/common/models/merchant.js
@@ -18,26 +18,61 @@ module.exports = function(Merchant) {
     let clientToUpdate = [];
     let clientToInsert = [];
     let i=0;
-    data.forEach((element,index)=>{
-      if (index===0){
-        let lastSync = new Date((element.lastUpdated));
-        try{
-          if (isNaN(lastSync)) throw BreakException;
-        } catch (e) {
-          callback("first object must be a valid date");
-          return;
-        }
-
-        console.log("On récupère la date");
-        console.log(typeof lastSync+lastSync);
-        Merchant.find({"where":{"creationDate" : {"gt": lastSync}}},function (err, response){
-          console.log("Erreur dans la recherche de nouvelles entrées\n"+err);
-          clientToInsert = response;
-          console.log("Résultat de la requête de populate du toInsert\n"+JSON.stringify(response));
-          i++;
-        });
+  // On récupère la date de dernière synchro du client
+      let lastSync = new Date((data.lastSync));
+      try{
+        if (isNaN(lastSync)) throw BreakException;
+      } catch (e) {
+        callback("first object must be a valid date");
+        return;
       }
-      else{
+
+      console.log("On récupère la date");
+      console.log(typeof lastSync+lastSync);
+      let promise2 = new Promise (function (resolve,reject){
+        Merchant.find({where:{creationDate : {gt: lastSync}}},function (err, response){
+          if (err){
+            callback(err);
+            return;
+          }
+          resolve(response);
+        });
+      });
+      promise2.then((value)=>{
+        clientToInsert = value;
+      });
+    console.log("Résultat de la requête de populate du toInsert\n"+JSON.stringify(clientToInsert));
+
+
+    if (data.localChanges.length===0){
+    //Il n'y a rien à updater, on renvoie les nouvelles entrées
+    callback(null,null,clientToInsert);
+    return;
+  }
+
+    /* Tentative de faire une recherche dans un array
+  let idList = [];
+  //On récupère les objets présent dans la liste
+    data.localChanges.forEach((merchant)=>{
+      idList.push(merchant.id);
+    });
+    let serverMerchantList = [];
+    let promise1 = new Promise (function (resolve,reject){
+      console.log("idList:\n"+idList);
+      Merchant.find({where:{id:{inq:["[\"cabc3a40-76f1-11e9-8f65-453b218fe36f\"]"]}}},function (response) {
+        console.log("recherche lancée : " +response);
+        resolve(response);
+      });
+    });
+
+    promise1.then((value)=>{
+      console.log("réponse de la recherche:\n"+value);
+    });
+*/
+
+    data.localChanges.forEach((element,index)=>{
+
+
         Merchant.findById(element.id, function (err, merchant) {
           console.log("-------------------");
           if (err) {
@@ -60,7 +95,7 @@ module.exports = function(Merchant) {
           }
           i++;
           //Lorsque toutes les actions sont terminées
-          if (i===data.length){
+          if (i===data.localChanges.length){
             //On fait un Bulk Insert
             if (toInsert.length>0){
               Merchant.create(toInsert, function(error, response){
@@ -88,7 +123,6 @@ module.exports = function(Merchant) {
             callback(null,clientToUpdate,clientToInsert);
           }
         });
-      }
     });
   };
 };
